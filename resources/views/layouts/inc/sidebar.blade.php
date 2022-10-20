d5c7ccd541027dfd5166a1b468ce120585943735
ZylalMinollari <zminollari@gmail.com>
2022-10-20 17:59:06 +0200

    Add product feature for admin pannel

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -4,8 +4,8 @@
         </a></div>
     <div class="sidebar-wrapper">
         <ul class="nav">
-            <li class="nav-item {{ Request::is('dashboard.html') ? 'active' : '' }}  ">
-                <a class="nav-link" href="./dashboard.html">
+            <li class="nav-item {{ Request::is('dashboard') ? 'active' : '' }}  ">
+                <a class="nav-link" href="{{ route('home.index') }}">
                     <i class="material-icons">dashboard</i>
                     <p>Dashboard</p>
                 </a>
@@ -20,6 +20,18 @@
                 <a class="nav-link" href="{{ route('category-create') }}">
                     <i class="material-icons">person</i>
                     <p>Add Category</p>
+                </a>
+            </li>
+            <li class="nav-item {{ Request::is('products') ? 'active' : '' }} ">
+                <a class="nav-link" href="{{ route('products-index') }}">
+                    <i class="material-icons">person</i>
+                    <p>Products</p>
+                </a>
+            </li>
+            <li class="nav-item {{ Request::is('create-product') ? 'active' : '' }} ">
+                <a class="nav-link" href="{{ route('product-create') }}">
+                    <i class="material-icons">person</i>
+                    <p>Add Products</p>
                 </a>
             </li>
             <li class="nav-item ">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\FrontendController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -33,4 +35,11 @@ Route::group(['middleware' => ['auth', 'isAdmin']], function () {
     Route::get('/edit-category/{id}',[CategoryController::class,'edit'])->name('category-edit');
     Route::put('/update-category{id}',[CategoryController::class,'update'])->name('category-update');
     Route::get('/delete-category/{id}',[CategoryController::class,'destroy'])->name('category-delete');
+
+    Route::get('/products',[ProductController::class,'index'])->name('products-index');
+    Route::get('/create-product',[ProductController::class,'create'])->name('product-create');
+    Route::post('/store-product',[ProductController::class,'store'])->name('product-store');
+    Route::get('/edit-product/{id}',[ProductController::class,'edit'])->name('product-edit');
+    Route::put('/update-product{id}',[ProductController::class,'update'])->name('product-update');
+    Route::get('/delete-product/{id}',[ProductController::class,'destroy'])->name('product-delete');
 });
