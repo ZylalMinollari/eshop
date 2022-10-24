a35a7743a1e5809b53d0beda7d5ebc7623243c89
ZylalMinollari <zminollari@gmail.com>
2022-10-24 16:17:58 +0200

    Integrate owl carusel for featured product

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,20 +9,44 @@
     <div class="py-5">
         <div class="container">
             <div class="row">
-                @foreach ( $featured_products as $product)
-                <div class="col-md-3">
-                    <div class="card">
-                        <img src="{{ asset('assets/uploads/products/'.$product->image) }}" alt="Product Image ">
-                        <div class="card-body">
-                            <h5>{{ $product->name }}</h5>
-                            <small>{{ $product->selling_price }}</small>
+<h2>Featured Products</h2>
+                <div class="owl-carousel featured-carousel owl-theme">
+                    @foreach ($featured_products as $product)
+                        <div class="item">
+                            <div class="card">
+                                <img src="{{ asset('assets/uploads/products/' . $product->image) }}" alt="Product Image ">
+                                <div class="card-body">
+                                    <h5>{{ $product->name }}</h5>
+                                    <span class="float-start">{{ $product->selling_price }}</span>
+                                    <span class="float-end"> <s>{{ $product->original_price }}</s></span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
-                @endforeach
-                
             </div>
         </div>
     </div>
 @endsection
 
+@section('scripts')
+    <script>
+        $('.featured-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots:false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+    </script>
+@endsection
